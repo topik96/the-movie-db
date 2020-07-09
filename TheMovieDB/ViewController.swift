96a7c8dad5cbe0652db91ddb7b0430caa4b9cfb4
topik <topikmujianto7@gmail.com>
2020-07-09 07:47:01 +0700

    fixing closure

---
+++ TheMovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by topik mujianto on 09/07/20.
+//  Copyright © 2020 Feebe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
